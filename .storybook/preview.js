73be46c64c71684efc8b8cf3cabadc609c93cf36
alextanhongpin <alextan220990@gmail.com>
2020-09-19 23:01:21 +0200

    chore(storybook): configure storybook addons and themes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,16 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
+const withThemeProvider = (Story, context) => {
+  const theme = context.globals.theme;
+  return (
+    <ThemeProvider theme={{ mode: theme }}>
+      <Story />
+    </ThemeProvider>
+  );
+};
+
+export const decorators = [withThemeProvider];
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 
@@ -5,6 +18,18 @@ export const parameters = {
   options: {
     storySort: {
       //method: "alphabetical",
+    },
+  },
+};
+
+export const globalTypes = {
+  theme: {
+    name: "Theme",
+    description: "Global theme for components",
+    defaultValue: "light",
+    toolbar: {
+      icon: "circlehollow",
+      items: ["light", "dark"],
     },
   },
 };
