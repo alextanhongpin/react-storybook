4d74dc9ef642d97fbb7b5d4cff8768d62ed01c1d
alextanhongpin <alextan220990@gmail.com>
2020-09-19 23:03:50 +0200

    chore(button): use styled components for button

--- src/stories/Button.js
+++ src/stories/Button.js
@@ -1,29 +1,90 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './button.css';
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import theme from "styled-theming";
+
+const buttonVariants = theme.variants("mode", "variant", {
+  primary: {
+    light: `
+      color: white;
+      background-color: #1ea7fd;
+    `,
+    dark: `
+      color: white;
+      background-color: black;
+    `,
+  },
+  ghost: {
+    light: `
+      color: #333;
+      background-color: transparent;
+      box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+    `,
+    dark: `
+      color: white;
+      background-color: black;
+      box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+    `,
+  },
+});
+
+const buttonSizes = theme.variants("mode", "size", {
+  small: {
+    light: `
+      font-size: 12px;
+      padding: 10px 16px;
+    `,
+    dark: `
+      font-size: 12px;
+      padding: 10px 16px;
+    `,
+  },
+  medium: {
+    light: `
+      font-size: 14px;
+      padding: 11px 20px;
+    `,
+    dark: `
+      font-size: 14px;
+      padding: 11px 20px;
+    `,
+  },
+  large: {
+    light: `
+      font-size: 16px;
+      padding: 12px 24px;
+    `,
+    dark: `
+      font-size: 16px;
+      padding: 12px 24px;
+    `,
+  },
+  default: {
+    light: "",
+    dark: "",
+  },
+});
 
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
-};
+export const Button = styled.button`
+  font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-weight: 700;
+  border: 0;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
+  ${buttonVariants};
+  ${buttonSizes};
+  background-color: ${(props) => props.backgroundColor};
+`;
 
 Button.propTypes = {
   /**
    * Is this the principal call to action on the page?
    */
-  primary: PropTypes.bool,
+  variant: PropTypes.oneOf(["primary", "ghost"]),
   /**
    * What background color to use
    */
@@ -31,7 +92,7 @@ Button.propTypes = {
   /**
    * How large should the button be?
    */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  size: PropTypes.oneOf(["small", "medium", "large"]),
   /**
    * Button contents
    */
@@ -44,7 +105,7 @@ Button.propTypes = {
 
 Button.defaultProps = {
   backgroundColor: null,
-  primary: false,
-  size: 'medium',
+  variant: "primary",
+  size: "medium",
   onClick: undefined,
 };
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -6,6 +6,18 @@ export default {
   title: "Example/Button",
   component: Button,
   argTypes: {
+    variant: {
+      control: {
+        type: "select",
+        options: ["primary", "ghost"],
+      },
+    },
+    size: {
+      control: {
+        type: "select",
+        options: ["small", "medium", "large"],
+      },
+    },
     backgroundColor: { control: "color" },
   },
 };
@@ -14,23 +26,23 @@ const Template = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  label: "Button",
+  variant: "primary",
+  children: "Button",
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: "Button",
+  children: "Button",
 };
 
 export const Large = Template.bind({});
 Large.args = {
   size: "large",
-  label: "Button",
+  children: "Button",
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: "small",
-  label: "Button",
+  children: "Button",
 };
